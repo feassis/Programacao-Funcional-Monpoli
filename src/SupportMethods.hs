d4a467ce7c25977b95579d00e2cb0e418b91cc29
Gigeck <guilherme.gigeck@gmail.com>
2023-08-27 19:30:27 -0300

    removing useless files

--- src/SupportMethods.hs
+++
@@ -1,7 +0,0 @@
-module SupportMethods (randomPair) where
-
-import System.Random
-
--- Função que gera um par de números inteiros aleatórios dentro dos limites fornecidos
-randomPair :: Int -> Int -> IO Int
-randomPair lowerBound upperBound = randomRIO (lowerBound, upperBound)
